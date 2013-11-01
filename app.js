4c7a2588af04a5a9d67b73d9249daf536e69f8e7
Renato <raeffray@gmail.com>
2013-11-01 18:30:35 -0200

    My bad. trying to fix the mess

--- app.js
+++ app.js
@@ -4,9 +4,8 @@
  */
 
 var express = require('express');
-var consolidate = require('consolidate');
-
-var routes = require('./routes');
+var routes = require('./routes/index');
+var data = require('./routes/data');
 
 var http = require('http');
 var path = require('path');
@@ -17,12 +16,11 @@ var Quotation = require("./model/schemas").Quotation;
 
 var app = express();
 
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('jade', consolidate.jade);
-app.engine('ejs', consolidate.ejs);
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -33,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
@@ -42,5 +40,5 @@ app.get('/data', data.map);
 
 
 http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
